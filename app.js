a025ce82cfbba3d403b3927b47635de51a5ee916
farmanullah1 <farmanullahansari999@gmail.com>
2025-12-25 21:03:55 +0500

    HTML

---
+++ app.js
@@ -0,0 +1,57 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelectorAll(".reset-btn");
+
+let turn0 = true;
+
+const winPatterns = [
+  [0, 1, 2], 
+  [0, 3, 6], 
+  [0, 4, 8], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [2, 4, 6], 
+  [3, 4, 5], 
+  [6, 7, 8], 
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box clicked");
+    if (turn0) {
+      box.innerText = "O";
+      turn0 = false;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    console.log(pattern);
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "" ) {
+        if (pos1Val === pos2Val && pos2Val === pos3Val ) {
+            console.log("winner",pos1Val);
+        }
+    } 
+  }};
+
+
+
+//     const [a, b, c] = pattern;
+//     if (
+//       boxes[a].innerText &&
+//       boxes[a].innerText === boxes[b].innerText &&
+//         boxes[a].innerText === boxes[c].innerText
+//     ) {
+//       alert(`Player ${boxes[a].innerText} wins!`);
+//       resetGame();
+//       return;
+//     }
+//     }
+// };
